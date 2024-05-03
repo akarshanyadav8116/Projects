e067d003d4eab3831ee8e895dc64151e4b8f4e15
akarshanyadav8116 <111714329+akarshanyadav8116@users.noreply.github.com>
2024-05-03 18:08:18 +0530

    Add files via upload

---
+++ src/components/Content.js
@@ -0,0 +1,23 @@
+const Content=()=>{
+    return(
+        <div className="HeroSection">
+            <div className="Matter">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>Your Feet Deserve the Best and we're here to help you with our shoes.
+                Your Feet Deserve the Best and we're here to help you with our shoes.</p>
+            <div className="hero-btn">
+                <table>
+                    <tr>
+                        <td><button>Shop Now</button></td>
+                        <td><button className="Category">Category</button></td>
+                    </tr>
+                </table>
+            </div>
+            </div>
+            <div className ="shoes">
+            <img height="100px" src="https://static.nike.com/a/images/t_default/840e5541-171b-463e-b6c1-4019755ad3a2/court-borough-low-recraft-big-kids-shoes-dDK0q4.png" alt="" />
+            </div>
+        </div>
+    )
+};
+export default Content;
